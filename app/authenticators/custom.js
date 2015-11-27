63d449ef56a2a79680d9efb7a45b50027fc5a0df
Michael Celona <michael@celona.local>
2015-11-27 09:41:00 -0500

    auth working

---
+++ app/authenticators/custom.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+import Base from 'ember-simple-auth/authenticators/base';
+
+export default Base.extend({
+  tokenEndpoint: 'http://localhost:3000/v1/signin',
+  
+  restore(data) {
+    //console.log( data.token )
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (!Ember.isEmpty(data.token)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
+  
+  authenticate(options) {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      Ember.$.ajax({
+        url: this.tokenEndpoint,
+        type: "POST",
+        data: JSON.stringify({
+          email: options.identification,
+          password: options.password
+          }),
+        contentType: 'application/json;charset=utf-8',
+        dataType: 'json'
+        }).then(function(response) {
+          //console.log( response.auth_token )
+          Ember.run(function() {
+            resolve({
+              token: response.auth_token
+            });
+          });
+        }, function(xhr, status, error) {
+          var response = xhr.responseText;
+          Ember.run(function() {
+            reject(response);
+          });
+        });
+    });
+  },
+  
+  invalidate(data) {
+    //console.log( data )
+    return Ember.RSVP.resolve();
+  }
+});
