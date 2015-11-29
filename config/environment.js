78f3c9e43d07f3264d64610a5f80d9f4649edf9f
Michael Celona <michael@celona.local>
2015-11-29 14:23:33 -0500

    fix modulePrefix name

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'auth',
+    modulePrefix: 'cyc_fe',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
