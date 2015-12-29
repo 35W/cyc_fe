56dc18dc13676c7184070a703f75a02195a7ac65
Ned <nedcampion@gmail.com>
2015-12-29 13:47:19 -0500

    Clean up

--- app/helpers/sort-qp.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function sortQP(params) {
-  let sort = params[0];
-  let currentSort = params[1].get('sort');
-  return sort === currentSort ? ("-"+sort) : sort;
-}
-
-export default Ember.Helper.helper(sortQP);
